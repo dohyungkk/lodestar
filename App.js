6f858d8d33b1b9bc10e7c80b1f6a0ab879d23be1
Kevin Kim <kdkim7676@gmail.com>
2021-05-14 03:28:53 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="About Lodestar"
+        onPress={() => navigation.navigate('Lodestar')}
+      />
+      <Text />
+      <Button
+        title="About App"
+        onPress={() => navigation.navigate('About')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function AboutLodestar() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Lodestar Screen</Text>
+    </View>
+  );
+}
+
+function AboutApp() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>About App Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Lodestar" component={AboutLodestar} />
+        <Stack.Screen name="About" component={AboutApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Button
+//           title="About Lodestar"
+//           onPress={() => Alert.alert('Simple Button pressed')}
+//         />
+//         <br />
+//         <Button
+//           title="About App"
+//           onPress={() => Alert.alert('Simple Button pressed')}
+//         />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
